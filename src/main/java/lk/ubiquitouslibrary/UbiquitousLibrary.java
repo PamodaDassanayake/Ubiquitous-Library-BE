a620de6a7568ca1387618a52c105de57d8f2561a
Pamoda <pamodad26@gmail.com>
2021-07-24 13:43:25 +0530

    Initial project structure from Spring Starter

---
+++ src/main/java/lk/ubiquitouslibrary/UbiquitousLibrary.java
@@ -0,0 +1,13 @@
+package lk.ubiquitouslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiquitousLibrary {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbiquitousLibrary.class, args);
+    }
+
+}
